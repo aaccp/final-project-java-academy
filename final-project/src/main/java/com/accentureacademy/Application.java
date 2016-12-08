f7186e454363eef40012c8ea389203c76bc5e9a6
Angel Carrillo <aa.ccp@outlook.com>
2016-12-08 12:23:52 -0600

    Update project template

--- final-project/src/main/java/com/accentureacademy/Application.java
+++ final-project/src/main/java/com/accentureacademy/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinalProjectApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FinalProjectApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
