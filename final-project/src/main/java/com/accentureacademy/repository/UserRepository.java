f63c6a892dd4458ea14e1794fac0cd714bea9f25
Angel Carrillo <aa.ccp@outlook.com>
2016-12-09 14:14:38 -0600

    Update UserRepository

--- final-project/src/main/java/com/accentureacademy/repository/UserRepository.java
+++ final-project/src/main/java/com/accentureacademy/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.accentureacademy.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.accentureacademy.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+	
+	List<User> findByName(String name);
 
 }
