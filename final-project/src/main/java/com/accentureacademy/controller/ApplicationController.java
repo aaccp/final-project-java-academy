d92a9e42296ef3838db7e56015b0ca1734f65a6f
Angel Carrillo <aa.ccp@outlook.com>
2016-12-11 21:20:25 -0600

    Update AplicationController to function as a RestController

--- final-project/src/main/java/com/accentureacademy/controller/ApplicationController.java
+++ final-project/src/main/java/com/accentureacademy/controller/ApplicationController.java
@@ -1,23 +1,50 @@
 package com.accentureacademy.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
 
-@Controller
-public class WelcomeController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.accentureacademy.model.User;
+import com.accentureacademy.repository.UserRepository;
+
+@RestController
+@SessionAttributes("user")
+@RequestMapping("api/v1/")
+public class ApplicationController {
+	
+	@Autowired
+	private UserRepository repo;
 
 	@RequestMapping("/welcome")
-	public String welcome(@RequestParam(value="name", required=false, defaultValue="") String name, Model model) {
+	public String welcome(@RequestParam(value="name", required=false, defaultValue="") String name) {
 		StringBuilder wMessage = new StringBuilder("Welcome");
 		if(name != null && !name.isEmpty()){
 			wMessage.append(", ");
 			wMessage.append(name);
 		}
 		wMessage.append("!");
-		model.addAttribute("message", wMessage.toString());
-		return "welcome";
+		return wMessage.toString();
 	}
 
+    @RequestMapping(value= "users", method= RequestMethod.POST)
+    public User addUser(@RequestBody User user){
+    	return repo.saveAndFlush(user);
+    }
+
+    @RequestMapping(value= "users/{id}", method = RequestMethod.GET)
+	public User getUser(@PathVariable Long id) {
+		return repo.findOne(id);
+	}
+
+    @RequestMapping(value = "users", method = RequestMethod.GET)
+	public List<User> usersList() {
+		return repo.findAll();
+	}
 }
