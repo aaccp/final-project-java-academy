73e00e7e5a61a404543f237b586cb54ca814347f
Angel Carrillo <aa.ccp@outlook.com>
2016-12-12 02:28:04 -0600

    Added Welcome.java model for rest service as object

---
+++ final-project/src/main/java/com/accentureacademy/model/Welcome.java
@@ -0,0 +1,18 @@
+package com.accentureacademy.model;
+
+public class Welcome {
+	private String wMessage;
+	
+	public Welcome(String wMessage){
+		this.wMessage = wMessage;
+	}
+
+	public String getwMessage() {
+		return wMessage;
+	}
+
+	public void setwMessage(String wMessage) {
+		this.wMessage = wMessage;
+	}
+
+}
