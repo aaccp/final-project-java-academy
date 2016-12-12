211facff19cf758139b3ffbe1dcdbe0ca5053e70
Angel Carrillo <aa.ccp@outlook.com>
2016-12-12 04:03:35 -0600

    Update ApplicationController.java

--- final-project/src/main/java/com/accentureacademy/controller/ApplicationController.java
+++ final-project/src/main/java/com/accentureacademy/controller/ApplicationController.java
@@ -2,6 +2,7 @@ package com.accentureacademy.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,14 +15,16 @@ import com.accentureacademy.repository.UserRepository;
 
 @RestController
 @RequestMapping("api/v1/")
-public class ApplicationController {
-	
+public class ApplicationController{
 	@Autowired
 	private UserRepository repo;
 
     @RequestMapping(value="users", method=RequestMethod.POST)
     public User addUser(@RequestBody User user){
-    	return repo.saveAndFlush(user);
+    	if(!alreadyExists(user))
+    		return repo.saveAndFlush(user);
+    	else
+    		return user;
     }
 
     @RequestMapping(value="users", method=RequestMethod.GET)
@@ -33,4 +36,27 @@ public class ApplicationController {
 	public User getUser(@PathVariable Long id) {
 		return repo.findOne(id);
 	}
+
+    @RequestMapping(value="users/{id}", method=RequestMethod.PUT)
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        User usr = repo.findOne(id);
+        BeanUtils.copyProperties(user, usr);
+        return repo.saveAndFlush(usr);
+    }
+
+    @RequestMapping(value="users/{id}", method=RequestMethod.DELETE)
+    public User deleteUser(@PathVariable Long id) {
+        User usr = repo.findOne(id);
+        repo.delete(usr);
+        return usr;
+    }
+    
+    private boolean alreadyExists(User user){
+    	List<User> users = this.usersList();
+    	for(User usr: users){
+    		if(usr.getName().compareTo(user.getName())==0)
+    			return true;
+    	}
+    	return false;
+    }
 }
